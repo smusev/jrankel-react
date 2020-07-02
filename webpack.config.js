2b77d0cb7de06c9afe582907c40699fb9ebc401b
Jon Smusev <you@example.com>
2020-07-02 19:19:34 +0300

    added babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
@@ -14,5 +14,6 @@ module.exports = {
         loader: 'babel-loader'
       }
     ]
-  }
+  },
+  devtool: 'cheap-module-eval-source-map'
 };
